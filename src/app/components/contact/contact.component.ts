a9936224bf6bd5c153263a611a24d398bff5809d
masato <masato.ngsm@gmail.com>
2020-07-12 19:40:10 +0900

    終わり

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { jsonServer } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  contacts = [
+    {
+      icon: `${jsonServer}/images/phone.svg`,
+      value: '0120-XXX-0000',
+    },
+    {
+      icon: `${jsonServer}/images/facebook.svg`,
+      value: 'https://facebook/xinchao/example.com',
+    },
+    {
+      icon: `${jsonServer}/images/email.svg`,
+      value: 'xinchao@example.com',
+    },
+  ];
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.contacts[0].icon);
   }
-
 }
